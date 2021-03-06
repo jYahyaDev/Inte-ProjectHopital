fb96d5a8a1467b5d25eb59eab5dff569b8c5b4a6
Jawad YAHYA <jawad.yahya30@gmail.com>
2021-03-06 12:59:27 +0100

    Début section about animation js ok ? ajout icon

--- public/Js/script.js
+++ public/Js/script.js
@@ -1,7 +1,5 @@
 const ElBtnServ = document.querySelectorAll('.g-serv__btn');
 
-
-
 function visible(){
         this.classList.toggle('visible')
     console.log(this)
@@ -15,6 +13,22 @@ function visible(){
 
     })
 
+/* INTERSECTION OBSERVER API */
+
+let observer = new IntersectionObserver(function(entries){
+    entries.forEach(entry=>{
+        if(entry.intersectionRatio>0.2){
+            entry.target.classList.remove('invisible');
+            entry.target.classList.add('animation');
+        }
+
+    })
+
+}, {
+    threshold: 0.5
+})
+
+let titre = document.querySelector('.g-about');
 
 
-
+observer.observe(titre);
